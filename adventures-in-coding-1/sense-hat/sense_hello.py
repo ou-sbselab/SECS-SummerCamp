ffe64dc05904a4141eedd4d6fad78a226165d2ac
Erik Fredericks <fredericks@oakland.edu>
2018-06-25 13:11:16 -0400

    Added Sense hat basics

---
+++ adventures-in-coding-1/sense-hat/sense_hello.py
@@ -0,0 +1,11 @@
+## OU SECS Summer Camp
+## Adventures in Coding 1
+## Summer 2018
+## Erik Fredericks
+## sense_hello.py
+
+## Now, it's time to do hello world on the Sense hat!
+from sense_hat import SenseHat
+sense = SenseHat()
+
+sense.show_message("Hello world!")
