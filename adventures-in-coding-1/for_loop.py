e23337b13dcb47cf85b70ac956045f87d007bad0
Erik Fredericks <fredericks@oakland.edu>
2018-06-25 00:50:07 -0400

    Added for loop demo

---
+++ adventures-in-coding-1/for_loop.py
@@ -0,0 +1,10 @@
+## OU SECS Summer Camp
+## Adventures in Coding 1
+## Summer 2018
+## Erik Fredericks
+## for_loop.py
+
+## A simple demo of a for loop.
+
+for i in range(0,100):
+  print "This is loop number %d" % i 
